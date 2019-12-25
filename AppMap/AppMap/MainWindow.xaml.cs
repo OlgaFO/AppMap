9bc67cdca6ac97ac8fb8672ca81cf3a7fc325926
Ольга Фесенко <Olga.Fesenko29@outlook.com>
2019-12-26 00:07:31 +0300

    Initial commit

---
+++ AppMap/AppMap/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
+namespace AppMap
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+        //    var cityCenter = new Geopoint(cityPosition);
+
+        //    await(sender as MapControl).TrySetViewAsync(cityCenter, 12);
+        //}
+    }
+}
